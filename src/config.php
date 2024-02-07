0d9ced3f9ab6996aeeae1df3c453a09cccd1a347
Jonathon Taylor <jonathont@berkeley.edu>
2024-02-07 14:23:51 -0800

    fix double-quoting when reading config

--- src/config.php
+++ src/config.php
@@ -13,22 +13,22 @@ $verify_peer = true;
 // Override defaults
 foreach (getenv() as $key => $value) {
     if ($key == 'ISSUER') {
-        $issuer = $value;
+        $issuer = str_replace('"','',$value);
     }
     if ($key == 'CID') {
-        $cid = $value;
+        $cid = str_replace('"','',$value);
     }
     if ($key == 'SECRET') {
-        $secret = $value;
+        $secret = str_replace('"','',$value);
     }
     if ($key == 'SERVER_CA_PATH') {
-        $cert_path = $value;
+        $cert_path = str_replace('"','',$value);
     }
     if ($key == 'UPGRADE_HTTP') {
         $upgrade_insecure_http_requests = filter_var($value, FILTER_VALIDATE_BOOLEAN);
     }
     if ($key == 'REDIRECT_URL') {
-        $redirect_url = $value;
+        $redirect_url = str_replace('"','',$value);
     }
     if ($key == 'VERIFY_HOST') {
         $verify_host = filter_var($value, FILTER_VALIDATE_BOOLEAN);
